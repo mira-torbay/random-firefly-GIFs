6487f04b36f234421e2426f4ba06e25706e653fe
Mira Torbay <mirandatorbay@gmail.com>
2023-01-22 16:43:47 -0500

    fixed CSP error (mostly)

--- scripts/chooseRandomGIF.js
+++ scripts/chooseRandomGIF.js
@@ -1,38 +1,48 @@
-function chooseGIF() {
+function randomGIF() {
+  const number = Math.floor(Math.random() * 8);
+  let text = "placeholder";
+  var gif;
+   switch(number) {
+      case 0:
+        text = "Jayne hat";
+        gif = "GIFs/jayne-hat.gif";
+        break;
+      case 1:
+        text = "Mal 'aim to misbehave'";  
+        gif = "GIFs/mal-misbehave.gif";
+        break;
+      case 2:
+        text = "Wash dinosaurs";  
+        gif = "GIFs/wash-dinosaurs.gif";
+        break
+      case 3:
+        text = "Mal bonnet";  
+        gif = "GIFs/mal-bonnet.gif";
+        break
+      case 4:
+        text = "Mal turbulence quote";
+        gif = "GIFs/mal-turbulence.gif";
+        break
+      case 5:
+        text = "Kaylee umbrella";
+        gif = "GIFs/kaylee-umbrella.gif";
+        break
+      case 6:
+        text = "Kaylee and Inara";
+        gif = "GIFs/kaylee-inara.gif";
+        break
+      case 7:
+        text = "River 'no power in the verse'"
+        gif = "GIFs/river-power.gif";
+        break
+      default:
+        text = "uh we have a problem";
+        gif = "GIFs/error.gif";
+    }
+    document.getElementById('myGIF').src = gif;
+  }
 
-// choosing a random number
-const number = Math.floor(Math.random() * 8);
-
-let gif = "placeholder";
-switch(number) {
-  case 0:
-    gif = "GIFs/jayne-hat.gif";
-    break;
-  case 1:
-    gif = "GIFs/mal-misbehave.gif";
-    break;
-  case 2:
-    gif = "GIFs/wash-dinosaurs.gif";
-    break
-  case 3:
-    gif = "GIFs/mal-bonnet.gif";
-    break
-  case 4:
-    gif = "GIFs/mal-turbulence.gif";
-    break
-  case 5:
-    gif = "GIFs/kaylee-umbrella.gif";
-    break
-  case 6:
-    gif = "GIFs/kaylee-inara.gif";
-    break
-  case 7:
-    gif = "GIFs/river-power.gif";
-    break
-  default:
-    gif = "uh we have a problem";
-}
-  
-  document.write("Selected number: ${number}, Selected GIF: ${gif}");
-  
-}
+const button = document.querySelector("button");
+button.addEventListener("click", async () => {
+  randomGIF();
+});
